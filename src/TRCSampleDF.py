c59eec1369ce3a34c3c32430b961701b401fa8d1
Hamish <Hamish@Stonyanalytics.onmicrosoft.com>
2025-05-12 19:38:05 +1200

    Added Windrose

--- src/TRCSampleDF.py
+++ src/TRCSampleDF.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def sites_dataframe():
     sites = {
-    "id": [8, 32, 33, 4, 42, 24],
-    "site": ["Kapoaiaia at Cape Egmont", "Stony at Mangatete Bridge", "Taungatara at Eltham Road", "Waiwhakaiho at Hillsborough", "Waitara at Bertrand Road", "North Egmont at Visitor Centre"],
+    "site": [8, 32, 33, 4, 42, 24],
+    "site_label": ["Kapoaiaia at Cape Egmont", "Stony at Mangatete Bridge", "Taungatara at Eltham Road", "Waiwhakaiho at Hillsborough", "Waitara at Bertrand Road", "North Egmont at Visitor Centre"],
     "wd": [True, False, True, True, False, False],
     "wg": [True, False, True, True, False, False],
     "ws": [True, False, True, True, False, False],
@@ -18,11 +18,20 @@ def sites_dataframe():
 
 def measures_dataframe():
     measures = {
-    "id": [10, 4, 2, 3, 1, 9, 7, 5],
-    "label": ["wg", "wd", "ws", "at", "rf", "cu", "ht", "sm"],
+    "measure": [10, 4, 2, 3, 1, 9, 7, 5],
+    "measure_label": ["wg", "wd", "ws", "at", "rf", "cu", "ht", "sm"],
     "description": ["Wind Gust", "Wind Direction", "Wind Speed", "Air Temperature", "Rainfall", "River Flow", "River Level", "Soil Moisture"],
     "weather": [True, True, True, True, True, False, False, False],
     "river": [False, False, False, False, True, True, True, True]
     }
     df_measures = pd.DataFrame(measures)
     return(df_measures)
+
+
+def timelinks_dataframe():
+    timelink = {
+    "id": [1, 2, 3],
+    "label": ["7days", "30days", "365days"]
+    }
+    df_timelink = pd.DataFrame(timelink)
+    return(df_timelink)
